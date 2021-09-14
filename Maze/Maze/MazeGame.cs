21f897343bc866f64017b3e6240f760568f5592a
Павел <crazy.digital.bird@gmail.com>
2021-09-14 19:57:58 +0300

    Impementation of the abstract method pattern in the 'Maze' application

--- Maze/Maze/MazeGame.cs
+++ Maze/Maze/MazeGame.cs
@@ -4,25 +4,45 @@
     {
         public Maze CreateMaze()
         {
-            Maze aMaze = new Maze();
-            Room r1 = new Room(1);
-            Room r2 = new Room(2);
-            Door theDoor = new Door(r1, r2);
+            Maze aMaze = MakeMaze();
+            Room r1 = MakeRoom(1);
+            Room r2 = MakeRoom(2);
+            Door theDoor = MakeDoor(r1, r2);
 
             aMaze.AddRoom(r1);
             aMaze.AddRoom(r2);
 
-            r1.SetSide(Direction.North, new Wall());
-            r1.SetSide(Direction.South, new Wall());
+            r1.SetSide(Direction.North, MakeWall());
+            r1.SetSide(Direction.South, MakeWall());
             r1.SetSide(Direction.East, theDoor);
-            r1.SetSide(Direction.West, new Wall());
+            r1.SetSide(Direction.West, MakeWall());
 
-            r2.SetSide(Direction.North, new Wall());
-            r2.SetSide(Direction.South, new Wall());
-            r2.SetSide(Direction.East, new Wall());
+            r2.SetSide(Direction.North, MakeWall());
+            r2.SetSide(Direction.South, MakeWall());
+            r2.SetSide(Direction.East, MakeWall());
             r2.SetSide(Direction.West, theDoor);
 
             return aMaze;
+        }
+        
+        public virtual Maze MakeMaze()
+        {
+            return new Maze();
+        }
+
+        public virtual Room MakeRoom(int number)
+        {
+            return new Room(number);
+        }
+
+        public virtual Wall MakeWall()
+        {
+            return new Wall();
+        }
+
+        public virtual Door MakeDoor(Room room1, Room room2)
+        {
+            return new Door(room1, room2);
         }
     }
 }
